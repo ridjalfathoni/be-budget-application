6bffb260cbefc2c7b68144b386daca3749ccc5da
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-28 16:35:52 +0700

    add pre hook transactionsSchema

--- models/transactions.models.js
+++ models/transactions.models.js
@@ -25,6 +25,83 @@ module.exports = (mongoose) => {
             default: Date.now
         }
     });
+
+    Transactions.pre('save', function (next) {
+        const transaction = this;
+        const Users = mongoose.models.Users
+
+        if (transaction.isNew) {
+            Users.findById(transaction.user_id, (err, user) => {
+                if (err) {
+                    next(err)
+                } else {
+                    if (transaction.type === 'income') {
+                        user.balance += transaction.amount;
+                    } else if (transaction.type === 'expense') {
+                        user.balance -= transaction.amount
+                    }
+                    user.save((err) => {
+                        if (err) {
+                            next(err);
+                        } else {
+                            next()
+                        }
+                    })
+                }
+            })
+        } else {
+            next()
+        }
+    })
+    Transactions.pre('deleteMany', async function(next) {
+        const Users = mongoose.models.Users;
+        const Transactions = mongoose.models.Transactions;
+        try {
+            const query = this.getQuery();
+            const deletedTransactions = await Transactions.find(query);
+            const user_id = deletedTransactions.map(transaction => transaction.user_id);
+            const users = await Users.find({ _id: { $in: user_id } });
+            for (let i = 0; i < deletedTransactions.length; i++) {
+                const user = users.find(user => user._id.equals(deletedTransactions[i].user_id));
+                if (deletedTransactions[i].type === "income") {
+                    user.balance -= deletedTransactions[i].amount;
+                } else if (deletedTransactions[i].type === 'expense') {
+                    user.balance += deletedTransactions[i].amount;
+                }
+            }
+            await Promise.all(users.map(user => user.save()));
+            next();
+        } catch (err) {
+            next(err);
+        }
+    });
+    Transactions.pre('findOneAndUpdate', async function(next) {
+        const Users = mongoose.models.Users;
+        const Transactions = mongoose.models.Transactions;
+        try {
+            const query = this.getQuery();
+            const deletedTransactions = await Transactions.find(query);
+            const user_id = deletedTransactions.map(transaction => transaction.user_id);
+            const users = await Users.find({ _id: { $in: user_id } });
+            for (let i = 0; i < deletedTransactions.length; i++) {
+                const user = users.find(user => user._id.equals(deletedTransactions[i].user_id));
+                if (deletedTransactions[i].type === "income") {
+                    user.balance -= deletedTransactions[i].amount;
+                } else if (deletedTransactions[i].type === 'expense') {
+                    if (Math.sign(user.balance) > 0) {
+                        user.balance -= deletedTransactions[i].amount;
+                    } else {
+                        user.balance += deletedTransactions[i].amount;
+                    }
+                }
+            }
+            await Promise.all(users.map(user => user.save()));
+            next();
+        } catch (err) {
+            // next(err);
+        }
+    });
+
     
     return mongoose.model('Transactions', Transactions);
 }
