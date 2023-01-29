9418a2b32c141c3cbd41c1c24867992f5d21987f
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-29 22:23:07 +0700

    route /getListWallet

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -2,5 +2,6 @@ const Wallet = require("../controllers/wallet.controllers.js")
 const Middleware = require('../middleware/verifyToken');
 module.exports = ((app,router) => {
     router.post('/addWallet', Middleware.verifyToken, Wallet.addWallet);
+    router.get('/getListWallet', Middleware.verifyToken, Wallet.getListWallet);
     app.use("/api/wallet", router);
 })
